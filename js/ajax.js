80a60be3f8d697f98ddc4b835a137326d13970d1
Anya <youremail@domain.com>
2020-03-30 19:44:16 +0300

    first

---
+++ js/ajax.js
@@ -0,0 +1,23 @@
+const ajax = {
+	get(endpoint) {
+		return fetch(endpoint).then(response => response.json());
+	},
+	post() {},
+	delete(endpoint) {
+		const options = {
+			method: 'DELETE'
+		};
+
+		return fetch(endpoint, options).then(response => response.json());
+	},
+	put(endpoint, body) {
+		const options = {
+			method: 'PUT',
+			body: JSON.stringify(body)
+		};
+
+		return fetch(endpoint, options).then(response => response.json());
+	}
+};
+
+export default ajax;
